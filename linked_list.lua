ea638190eec1d012cefc240b80297583f01db8c7
wherewindblow <wherewindblow@sina.com>
2018-09-01 17:06:46 +0800

    feat: add linked list and queue

---
+++ linked_list.lua
@@ -0,0 +1,223 @@
+local LinkedList = Object:inherit("LinkedList")
+
+function LinkedList:constructor()
+	self.m_head = {}
+	self.m_tail = {}
+	self.m_head.next = self.m_tail
+	self.m_tail.previous = self.m_head
+	self.m_size = 0
+end
+
+function LinkedList:add(value)
+	self:pushBack(value)
+end
+
+function LinkedList:pushFront(value)
+	local newNode = { value = value, previous = self.m_head, next = self.m_head.next }
+	self.m_head.next.previous = newNode
+	self.m_head.next = newNode
+	self.m_size = self.m_size + 1
+end
+
+function LinkedList:pushBack(value)
+	local newNode = { value = value, previous = self.m_tail.previous, next = self.m_tail }
+	self.m_tail.previous.next = newNode
+	self.m_tail.previous = newNode
+	self.m_size = self.m_size + 1
+end
+
+function LinkedList:remove(value)
+	assert(value, "value cannot be nil")
+	local node = self.m_head
+	while node do
+		if node.value == value then
+			node.previous.next = node.next
+			node.next.previous = node.previous
+			self.m_size = self.m_size - 1
+			return
+		end
+		node = node.next
+	end
+end
+
+function LinkedList:removeFront()
+	if self:empty() then
+		return
+	end
+
+	local node = self.m_head.next
+	self.m_head.next = node.next
+	node.next.previous = self.m_head
+	self.m_size = self.m_size - 1
+end
+
+function LinkedList:removeBack()
+	if self:empty() then
+		return
+	end
+
+	local node = self.m_tail.previous
+	self.m_tail.previous = node.previous
+	node.previous.next = self.m_tail
+	self.m_size = self.m_size - 1
+end
+
+function LinkedList:removeIf(isTarget)
+	local node = self.m_head
+	while node do
+		if node.value and isTarget(node.value) then
+			node.previous.next = node.next
+			node.next.previous = node.previous
+			self.m_size = self.m_size - 1
+		end
+		node = node.next
+	end
+end
+
+function LinkedList:size()
+	return self.m_size
+end
+
+function LinkedList:iterator()
+	local next = self.m_head.next
+	local function nextValue()
+		local value = next.value
+		next = next.next
+		return value
+	end
+
+	return nextValue
+end
+
+function LinkedList:empty()
+	return self.m_size == 0
+end
+
+function LinkedList:debug()
+	local node = self.m_head
+	while node do
+		print(node, node.value, node.previous, node.next)
+		node = node.next
+	end
+end
+
+local function test()
+	local list = LinkedList:new()
+	assert(list:empty())
+
+	list:pushBack(1)
+	list:pushBack(2)
+	assert(not list:empty() and list:size() == 2)
+
+	local iterator = list:iterator()
+	local currentValue = 1
+	while true do
+		local value = iterator()
+		if not value then
+			break
+		end
+
+		assert(currentValue == value)
+		currentValue = currentValue + 1
+	end
+
+	list:removeFront()
+	assert(list:size() == 1 and list:iterator()() == 2)
+
+	list:pushFront(1)
+	list:removeBack()
+	assert(list:size() == 1 and list:iterator()() == 1)
+end
+
+test()
+
+local function computeUseTime(addFunc, removeFunc)
+	local container
+	local function proccess(funcName, func)
+		collectgarbage("collect")
+		local startMem = collectgarbage("count")
+		local startTime = os.clock()
+		container = func(container) or container
+		local finishTime = os.clock()
+		collectgarbage("collect")
+		local finishMem = collectgarbage("count")
+		print(string.format("%10s use time  %0.4f,   mem %7d", funcName, finishTime - startTime, finishMem - startMem))
+	end
+
+	proccess("addFunc", addFunc)
+	proccess("removeFunc", removeFunc)
+end
+
+local function compareArrayAndList()
+	local ITEM_NUM = 14000
+
+	-- Hold integer.
+	computeUseTime(function()
+		local t = {}
+		for i = 1, ITEM_NUM do
+			table.insert(t, i)
+		end
+		return t
+	end, function(t)
+		for i = 1, ITEM_NUM do
+			table.remove(t, 1)
+		end
+	end)
+
+	computeUseTime(function()
+		local list = LinkedList:new()
+		for i = 1, ITEM_NUM do
+			list:pushBack(i)
+		end
+		return list
+	end, function(list)
+		for i = 1, ITEM_NUM do
+			list:removeFront()
+		end
+	end)
+
+	-- Hold table.
+	computeUseTime(function()
+		local t = {}
+		for i = 1, ITEM_NUM do
+			table.insert(t, { i })
+		end
+		return t
+	end, function(t)
+		for i = 1, ITEM_NUM do
+			table.remove(t, 1)
+		end
+	end)
+
+	computeUseTime(function()
+		local list = LinkedList:new()
+		for i = 1, ITEM_NUM do
+			list:pushBack({ i })
+		end
+		return list
+	end, function(list)
+		for i = 1, ITEM_NUM do
+			list:removeFront()
+		end
+	end)
+end
+
+--compareArrayAndList()
+
+-- Hold integer.
+--
+-- use array.    addFunc use time  0.0000,   mem     256
+-- use array. removeFunc use time  1.1000,   mem       0
+--
+-- use list.     addFunc use time  0.0100,   mem    3063
+-- use list.  removeFunc use time  0.0000,   mem   -3062
+--
+-- Hold table.
+--
+-- use array.    addFunc use time  0.0000,   mem    1349
+-- use array. removeFunc use time  1.1200,   mem   -1093
+--
+-- use list.     addFunc use time  0.0100,   mem    4157
+-- use list.  removeFunc use time  0.0100,   mem   -4156
+
+return LinkedList
---
+++ queue.lua
@@ -0,0 +1,62 @@
+local LinkedList = require("linked_list")
+local Queue = Object:inherit("Queue")
+
+function Queue:constructor()
+	self.m_list = LinkedList:new()
+end
+
+function Queue:push(value)
+	self.m_list:add(value)
+end
+
+function Queue:front()
+	local iterator = self.m_list:iterator()
+	return iterator()
+end
+
+function Queue:pop()
+	self.m_list:removeFront()
+end
+
+function Queue:iterator()
+	return self.m_list:iterator()
+end
+
+function Queue:debug()
+	self.m_list:debug()
+end
+
+function Queue:size()
+	return self.m_list:size()
+end
+
+function Queue:empty()
+	return self.m_list:empty()
+end
+
+local function test()
+	local queue = Queue:new()
+	queue:push(1)
+	queue:push(2)
+	assert(not queue:empty() and queue:size() == 2)
+	assert(queue:front() == 1)
+
+	local iterator = queue:iterator()
+	local currentValue = 1
+	while true do
+		local value = iterator()
+		if not value then
+			break
+		end
+
+		assert(currentValue == value)
+		currentValue = currentValue + 1
+	end
+
+	queue:pop()
+	assert(queue:size() == 1 and queue:iterator()() == 2)
+end
+
+test()
+
+return Queue

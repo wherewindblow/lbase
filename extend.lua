4787dccc4e1537ea4342a31333746d5362c4faad
wherewindblow <wherewindblow@sina.com>
2018-09-01 16:37:29 +0800

    feat: add general size method of table

--- extend.lua
+++ extend.lua
@@ -1,3 +1,11 @@
+function table.size(t)
+	local count = 0
+	for k, v in pairs(t) do
+		count = count + 1
+	end
+	return count
+end
+
 function table.clone(src, dest)
 	dest = dest or {}
 	for k, v in pairs(src) do

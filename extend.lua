16095e25dfb1d28da489790f6d91ceb3b2a7bbea
wherewindblow <wherewindblow@sina.com>
2019-01-28 18:10:06 +0800

    feat: support to show local variables with traceback

--- extend.lua
+++ extend.lua
@@ -1,3 +1,12 @@
+-- Optimize.
+local type = type
+local pairs = pairs
+local next = next
+local tostring = tostring
+local assert = assert
+local format = string.format
+local len = string.len
+
 ---
 --- Returns size of table.
 --- @param t table
@@ -78,4 +87,74 @@ end
 --- @return table
 function string.totable(s)
 	return loadstring("return ".. s)()
+end
+
+local function addLocalVariable(str, level)
+	level = level + 1 -- Outside level.
+	local localNum = 1
+	while true do
+		local name, value = debug.getlocal(level, localNum)
+		if not name then
+			break
+		end
+
+		-- Add variable name.
+		str = format("%s\n\t\t'%s'", str, name)
+
+		-- Add variable value.
+		local valueType = type(value)
+		if valueType == "number" then
+			str = format("%s: %s", str, tostring(value))
+		elseif valueType == "string" then
+			str = format("%s: '%s'", str, tostring(value))
+		else
+			str = format("%s: %s", str, tostring(value))
+		end
+
+		localNum = localNum + 1
+	end
+	return str
+end
+
+---
+--- Returns full traceback message that include all local variables.
+--- @param level number
+--- @return string
+function debug.fulltraceback(level)
+	level = level or 2 -- Outside level is 2.
+	local str = "stack traceback:"
+
+	while true do
+		local funcInfo = debug.getinfo(level)
+		if not funcInfo then
+			break
+		end
+
+		-- Add source file.
+		str = format("%s\n\t(%d) %s:", str, level - 1, funcInfo.short_src)
+
+		-- Add source line.
+		if funcInfo.currentline > 0 then
+			str = format("%s%d:", str, funcInfo.currentline)
+		end
+
+		-- Add function name.
+		if len(funcInfo.namewhat) ~= 0 then
+			str = format("%s in function '%s'", str, funcInfo.name or "?")
+		else
+			if funcInfo.what == "main" then
+				str = format("%s in main chunk", str)
+			elseif funcInfo.what == "C" or funcInfo.what == "tail" then -- C function or tail call.
+				str = format("%s ?", str)
+			else
+				str = format("%s in function <%s:%d>", str, funcInfo.short_src, funcInfo.linedefined)
+			end
+		end
+
+		-- Add all local variable.
+		str = addLocalVariable(str, level)
+
+		level = level + 1
+	end
+	return str
 end

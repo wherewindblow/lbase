207764eb592f0c0a639b6679f321fef822ed509a
wherewindblow <wherewindblow@sina.com>
2019-07-07 19:11:10 +0800

    refactor: simplify event system

--- event.lua
+++ event.lua
@@ -1,100 +1,85 @@
 
 local Event = {
-	m_listenList = {},
+	m_eventList = {},
 }
 
 ---
 --- Register event for object. Only after this operation can use `listen` and `dispatch`.
---- @param obj table Object or module that will dispatch event.
 --- @param eventName string Name of event.
-function Event:register(obj, eventName)
-	local listenList = self.m_listenList[obj]
-	if not listenList then
-		listenList = {}
-		self.m_listenList[obj] = listenList
-	end
-
-	local observerList = listenList[eventName]
-	assert(not observerList, "Already register for this event.")
-	listenList[eventName] = {}
+function Event:register(eventName)
+	assert(not self.m_eventList[eventName], "Already register for this event.")
+	self.m_eventList[eventName] = {}
 end
 
 ---
 --- Removes event for object.
---- @param obj table Object or module that will dispatch event.
 --- @param eventName string Name of event. Default is nil to remove all event of object.
-function Event:remove(obj, eventName)
-	local listenList = self.m_listenList[obj]
-	if not listenList then
-		return
-	end
-
-	if eventName then
-		listenList[eventName] = nil
-	else
-		self.m_listenList[obj] = nil
-	end
+function Event:remove(eventName)
+	self.m_eventList[eventName] = nil
 end
 
 ---
---- Listens for event, when target object dispatch will call object callback.
+--- Listens for event, when dispatch will call object callback.
 --- NOTE: Must call `register` to register event before listen it.
 --- @param obj table  Object or module that listen event.
---- @param targetObj table Object or module that dispatch event.
 --- @param eventName string Name of event.
---- @param callbackName string Name of callback function that in `obj`. Like fun(targetObj, eventName, eventArgs).
-function Event:listen(obj, targetObj, eventName, callbackName)
-	local listenList = self.m_listenList[targetObj]
-	assert(listenList, "Listen for unknown event.")
-	local observerList = listenList[eventName]
-	assert(observerList, "Listen for unknown event.")
-	assert(not observerList[obj], "Already listen for this event.")
-	observerList[obj] = callbackName
+--- @param callbackName string Name of callback function that in `obj`. Like fun(eventName, ...).
+---                     This king of usage is use to support update function.
+function Event:listen(obj, eventName, callbackName)
+	local listenerList = self.m_eventList[eventName]
+	assert(listenerList, "Listen for unknown event.")
+	assert(not listenerList[obj], "Already listen for this event.")
+	listenerList[obj] = callbackName
 end
 
 ---
 --- Dispatches event to all object that listen for this event.
 --- NOTE: Must call `register` to register event before dispatch it.
---- @param obj table  Object or module that dispatch event.
 --- @param eventName string Name of event.
 --- @param eventArgs table Arguments about event.
-function Event:dispatch(obj, eventName, eventArgs)
-	local listenList = self.m_listenList[obj]
+function Event:dispatch(eventName, eventArgs)
+	local listenList = self.m_eventList[eventName]
 	assert(listenList, "Dispatch for unknown event.")
-	local observerList = listenList[eventName]
-	assert(observerList, "Dispatch for unknown event.")
-
-	for targetObj, callbackName in pairs(observerList) do
-		xpcall(function () targetObj[callbackName](targetObj, obj, eventName, eventArgs) end, debug.errorhook)
+	for targetObj, callbackName in pairs(listenList) do
+		xpcall(function()
+			if eventArgs then
+				targetObj[callbackName](targetObj, eventName, unpack(eventArgs))
+			else
+				targetObj[callbackName](targetObj, eventName)
+			end
+		end, debug.errorhook)
 	end
 end
 
 local function test()
-	local Src = {}
-	Event:register(Src, "work")
+	Event:register("work")
 
+	local Src = {}
 	function Src:work(...)
-		Event:dispatch(Src, "work", {...})
+		Event:dispatch("work", { Src, ... })
 	end
 
 	local Dest = {}
-	Event:listen(Dest, Src, "work", "onWork")
+	Event:listen(Dest, "work", "onWork")
+
+	local eventArgs = { 1, 2, "s" }
 
 	local finishEvent
-	function Dest:onWork(obj, name, args)
+	function Dest:onWork(eventName, eventSrc, ...)
 		finishEvent = true
-		assert(obj == Src)
-		assert(name == "work")
-		assert(args[1] == 1)
-		assert(args[2] == 2)
-		assert(args[3] == "s")
+		assert(eventName == "work")
+		assert(eventSrc == Src)
+		local args = { ... }
+		for i, value in ipairs(args) do
+			assert(value == eventArgs[i])
+		end
 	end
 
-	Src:work(1, 2, "s")
+	Src:work(unpack(eventArgs))
 	assert(finishEvent)
 
-	Event:remove(Src)
-	assert(table.empty(Event.m_listenList))
+	Event:remove("work")
+	assert(table.empty(Event.m_eventList))
 end
 
 test()

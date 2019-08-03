3ea7caee701597212430981c11d44dc03a7f6dd4
wherewindblow <wherewindblow@sina.com>
2019-08-03 23:43:46 +0800

    refactor: separate printing of table to extend

--- lbase/extend.lua
+++ lbase/extend.lua
@@ -6,6 +6,7 @@ local tostring = tostring
 local assert = assert
 local format = string.format
 local stringlen = string.len
+local stringsub = string.sub
 local tableinsert = table.insert
 
 ---
@@ -62,11 +63,81 @@ function table.deepclone(src, dest)
 	return clone(src, dest, 0)
 end
 
+local function valueString(any)
+	local str
+	local vType = type(any)
+	if vType == "string" then
+		str = format("\"%s\"", any)
+	elseif vType == "function" then
+		str = stringsub(tostring(any), 11)
+	elseif vType == "table" then
+		str = stringsub(tostring(any), 8)
+	else
+		str = tostring(any)
+	end
+	return str
+end
+
+local typeTagList = {
+	["boolean"] = "b",
+	["number"] = "n",
+	["string"] = "s",
+	["function"] = "f",
+	["thread"] = "th"
+}
+
+local function typeTag(any)
+	return typeTagList[type(any)] or type(any)
+end
+
+local printSetting = {
+	maxDeep = 100,
+	index = "    ",
+	defaultRootName = "root"
+}
+
 ---
 --- Prints table.
 --- @param t table
 function table.print(t)
-	printAny(t)
+	local printed = {}
+	local function innerPrint(any, deep, name)
+		if deep > printSetting.maxDeep then
+			return
+		end
+
+		if type(any) ~= "table" then
+			print(any)
+			return
+		end
+
+		local index = printSetting.index
+		local outIndex = string.rep(index, deep)
+		if deep == 1 then
+			print(format("%s %s", outIndex, valueString(any)))
+		end
+		print(format("%s {", outIndex))
+
+		local inIndex = outIndex .. index
+		for k, v in pairs(any) do
+			if type(v) ~= "table" then
+				print(format("%s[ %s ]%s = [ %s ]%s", inIndex, valueString(k), typeTag(k), valueString(v), typeTag(v)))
+			else
+				if not printed[v] then
+					local vName = format("%s.%s", name, tostring(k))
+					printed[v] = vName
+					print(format("%s[ %s ]%s = %s", inIndex, valueString(k), typeTag(k), valueString(v)))
+					innerPrint(v, deep + 1, vName)
+				else
+					print(format("%s[ %s ]%s = [ %s ]r", inIndex, valueString(k), typeTag(k), printed[v]))
+				end
+			end
+		end
+		print(format("%s }", outIndex))
+	end
+
+	printed[t] = printSetting.defaultRootName
+	innerPrint(t, 1, printed[t])
 end
 
 ---
--- lbase/global.lua
+++ lbase/global.lua
@@ -1,87 +1,14 @@
 -- Optimize.
-local type = type
-local tostring = tostring
 local print = print
 local error = error
 local assert = assert
 local format = string.format
-local stringsub = string.sub
-
-local function valueString(any)
-	local str
-	local vType = type(any)
-	if vType == "string" then
-		str = format("\"%s\"", any)
-	elseif vType == "function" then
-		str = stringsub(tostring(any), 11)
-	elseif vType == "table" then
-		str = stringsub(tostring(any), 8)
-	else
-		str = tostring(any)
-	end
-	return str
-end
-
-local function typeTag(any)
-	local tags = {
-		boolean = "b",
-		number = "n",
-		string = "s",
-		["function"] = "f",
-		thread = "th"
-	}
-	return tags[type(any)] or type(any)
-end
-
-local printSetting = {
-	maxDeep = 100,
-	index = "    ",
-	defaultRootName = "root"
-}
 
 ---
 --- Prints any value.
 function printAny(...)
-	local printed = {}
-	local function innerPrint(any, deep, name)
-		if deep > printSetting.maxDeep then
-			return
-		end
-
-		if type(any) ~= "table" then
-			print(any)
-			return
-		end
-
-		local index = printSetting.index
-		local outIndex = string.rep(index, deep)
-		if deep == 1 then
-			print(format("%s %s", outIndex, valueString(any)))
-		end
-		print(format("%s {", outIndex))
-
-		local inIndex = outIndex .. index
-		for k, v in pairs(any) do
-			if type(v) ~= "table" then
-				print(format("%s[ %s ]%s = [ %s ]%s", inIndex, valueString(k), typeTag(k), valueString(v), typeTag(v)))
-			else
-				if not printed[v] then
-					local vName = format("%s.%s", name, tostring(k))
-					printed[v] = vName
-					print(format("%s[ %s ]%s = %s", inIndex, valueString(k), typeTag(k), valueString(v)))
-					innerPrint(v, deep + 1, vName)
-				else
-					print(format("%s[ %s ]%s = [ %s ]r", inIndex, valueString(k), typeTag(k), printed[v]))
-				end
-			end
-		end
-		print(format("%s }", outIndex))
-	end
-
 	for _, v in ipairs({...}) do
-		printed = {}
-		printed[v] = printSetting.defaultRootName
-		innerPrint(v, 1, printed[v])
+		table.print(v)
 	end
 end
 
